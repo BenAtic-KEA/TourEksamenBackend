23a481409852ee200747fef61de89f31303f66dd
Mark Hansen <Mark024e@stud.kea.dk>
2022-05-30 11:43:55 +0200

    minor change repo

--- src/main/java/com/example/eksamen_backend/repositories/RiderRepository.java
+++ src/main/java/com/example/eksamen_backend/repositories/RiderRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface RiderRepository extends JpaRepository<Rider, Integer> {
-    List<Rider> findRidersByTeam(String team);
+    List<Rider> findRidersByTeamId(int teamId);
 }
